5af95da2c2320aa9852bc9d3f924727da3358227
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-14 22:04:37 -0400

    Creates Alert component for register page so that the user gets feedback on the success or failure of the login/register process

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,9 @@
 import Logo from "./Logo";
 import FormRow from "./FormRow";
+import Alert from "./Alert";
 
-export  {Logo, FormRow}
+export  {
+  Logo, 
+  FormRow,
+  Alert,
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { Logo, FormRow } from "../components"
+import { Logo, FormRow, Alert } from "../components"
 import Wrapper from '../assets/wrappers/RegisterPage'
 
 const initialState  = {
@@ -7,6 +7,7 @@ const initialState  = {
   email: '',
   password: '',
   isMember: true,
+  showAlert: false,
 }
 
 const Register = () => {
@@ -27,11 +28,27 @@ const Register = () => {
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
-        <h3>Logo</h3>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">name</label>
-          <input type="text" value={values.name} onChange={handleChange} className="form-input" />
-        </div>
+        <h3>Login</h3>
+        {values.showAlert && <Alert />}
+        <FormRow
+          type='text'
+          name='username'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        
         <button type="submit" className="btn btn-block">Submit</button>
       </form>
     </Wrapper>
