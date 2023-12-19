59db7bd92ad058e55b3ee0d77b29bf2efbde5c4a
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-19 01:27:23 -0400

    Set up libraries to handle errors catch and errors status code

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,9 @@
 import User from "../models/User.js"
+import { StatusCodes } from "http-status-codes"
 
-const register = async (req, res, next) => {
-  try {
+const register = async (req, res) => {
     const user = await User.create(req.body)
-    res.status(201).json({ user })
-  } catch (error) {
-    next(error)
-  }
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
