d0d6649a7876fe77628936f3993645a5adda27c0
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-21 12:21:21 -0400

    Set up nested pages and relative paths from home

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 // import Landing from "./pages/Landing";
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
-import {Landing, Register, Dashboard, Error} from './pages'
+import {Landing, Register, Error} from './pages'
+import { AddJob, AllJobs, Profile, SharedLayout, Stats } from './pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/">
+          <Route path='/stats' element={<Stats />} />
+          <Route path='all-jobs' element={<AllJobs />} />
+          <Route path='add-job' element={<AddJob />} />
+          <Route path='profile' element={<Profile />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
