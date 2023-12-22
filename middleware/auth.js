f0860877a3a44e763f8597b0a4262c4e056cc47b
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-22 13:31:55 -0400

    Finish setting up functionalities to restrict resources to non authenticated user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,22 @@
+import { UnAuthenticatedError } from "../errors/index.js";
+import jwt from "jsonwebtoken";
+
 const auth = async (req, res, next) => {
   const authHeader =  req.headers.authorization
-  console.log(authHeader);
-  next()
+  if(!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnAuthenticatedError('Authentication Invalid')
+  }
+  const token = authHeader.split(' ')[1]
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    // Attach the request object
+    // req.user = payload  --one way to do it
+    req.user = { userId: payload.userId }  // another way
+    next()
+  } catch (error) {
+    throw new UnAuthenticatedError('Authentication Invalid')
+    
+  }
 }
 
 export default auth
