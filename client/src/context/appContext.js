d3b9f0b1915543a31631300b7a65de48c1ecddaa
Johny270 <90469202+Johny270@users.noreply.github.com>
2023-12-26 14:15:53 -0400

    Define main structure for the job component and stub up logic to handle edit and delete a specific job

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -210,8 +210,15 @@ const AppProvider = ({children}) => {
     clearAlert()
   }
 
+  const setEditJob = (id) => {
+    console.log(`set edit job: ${id}`);
+  }
+  const deleteJob = (id) => {
+    console.log(`delete job: ${id}`)
+  }
+
   return (
-    <AppContext.Provider value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser, updateUser, handleChange, clearValues, createJob, getJobs }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert, setupUser, toggleSidebar, logoutUser, updateUser, handleChange, clearValues, createJob, getJobs, setEditJob, deleteJob }}>{children}</AppContext.Provider>
   ) 
 }
 
